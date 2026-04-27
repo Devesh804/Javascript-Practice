60816747bec4031dc760d1b59160b9597e55d2e1
Devesh804 <deveshc280@gmail.com>
2026-04-27 17:34:52 +0530

    29

--- Basic Problems (Grok)/Higher Order Functions/29.js
+++ Basic Problems (Grok)/Higher Order Functions/29.js
@@ -5,3 +5,29 @@
 // If operation = "add", it returns a function 
 // that adds two numbers
 // If "multiply", it returns a function that multiplies
+
+
+function calculator(operation) {
+    if (operation === "add") {
+        return function(a, b) {
+            return a + b;
+        };
+    }
+    else if (operation === "multiply") {
+        return function(a,b) {
+            return a*b;
+        };
+    }
+    else {
+        return function() {
+            return "Invalid operator"
+        };
+    }
+} 
+
+const add = calculator("add");
+console.log(add(2, 3));  
+
+
+const multiply = calculator("multiply");
+console.log(multiply(4, 5));
